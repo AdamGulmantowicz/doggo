f8ca978e527146fdb040d7b35306b375b7e362d8
“Your <janusssz@yahoo.com>
2022-06-14 21:14:18 +0200

    first task

--- js/index.js
+++ js/index.js
@@ -1,1 +1,22 @@
-console.log("Witaj świecie!");
+import data from "../data/data.js";
+
+const {
+  breedImages
+} = data;
+const dogs = JSON.parse(breedImages);
+const keys = Object.keys(dogs);
+
+
+keys.forEach((key) => {
+  var z = document.createElement("option");
+  z.setAttribute("class", `"mySelect__rase"`)
+  var t = document.createTextNode(`${key}`);
+  z.appendChild(t);
+  document.getElementById("mySelect").appendChild(z);
+  const dog = document.querySelector(".mySelect__rase");
+
+  // const rasePicture = document.querySelector(".mySelect");
+  // rasePicture.addEventListener('change', function () {
+
+  // })
+});

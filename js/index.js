0739d00f0f015fb7b399fb33f7af642801fc1192
TomaszFeszter <tomek.feszter.grupa@gmail.com>
2022-06-15 01:53:43 +0200

    add select js render

--- js/index.js
+++ js/index.js
@@ -1,1 +1,19 @@
-console.log("Witaj świecie!");
+import data from "../data/data.js";
+
+const breedList = JSON.parse(data.breedList);
+const breedImages = JSON.parse(data.breedImages);
+const main = document.querySelector('main');
+
+const renderForm = function () {
+    main.innerHTML = '<select name="breed" id="select" form="dogs"></select>';
+}
+renderForm();
+
+const renderOption = function () {
+    const select = document.getElementById('select');
+
+    select.innerHTML = Object.keys(breedList.message).map(function (element) {
+        return `<option value="${element}">${element}</option>`
+    }).join('')
+}
+renderOption();

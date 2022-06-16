ddf13510da2cf1d057449180992d00baad0b7a6e
Patrixion <patryk.kozielowicz@gmail.com>
2022-06-16 20:42:31 +0200

    drugie zadanie

--- js/index.js
+++ js/index.js
@@ -1,7 +1,34 @@
 import breedList from "../data/data.js";
-const input = document.getElementById("rodzajRasy");
+const input = document.getElementById("raceType");
+const formSelect = document.getElementById("formSelect");
+const dogDisplay = document.getElementsByClassName("dogPhotos");
+const submitButton = document.getElementById("submitButton");
+const printedDogs = document.getElementById("main");
 const breedAllList = JSON.parse(JSON.stringify(breedList));
 const breedingList = JSON.parse(breedAllList.breedList);
-const {message, status} = breedingList;
+
+const {
+    message,
+    status
+} = breedingList;
+
 const dogNames = Object.keys(message);
-dogNames.forEach(el => input.innerHTML +=`<option>${el}</option>`);
+dogNames.forEach(el => input.innerHTML += `<option value="${el}">${el}</option>`);
+
+function printingDogo(selectedDog) {
+    const breedImages = JSON.parse(breedAllList.breedImages);
+    for (const dog in breedImages) {
+        if (dog === selectedDog) {
+            for (const dogImage in breedImages[dog]) {
+                printedDogs.innerHTML += `<img src="${breedImages[dog][dogImage]}" alt="${breedImages[dog]}" srcset="">`;
+            }
+        }
+    }
+}
+
+formSelect.addEventListener("submit", e => {
+    e.preventDefault();
+    const selectedDog = input.value;
+    printedDogs.innerHTML = ``;
+    printingDogo(selectedDog);
+})

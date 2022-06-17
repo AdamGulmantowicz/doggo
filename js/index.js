26093f61edde994e7f7be033783e2889984747f0
Adam Gulmantowicz <adam200502@interia.pl>
2022-06-17 18:47:36 +0200

    Add fixes for image display

--- js/index.js
+++ js/index.js
@@ -3,21 +3,23 @@ import data from "../data/data.js";
 const dogsList = JSON.parse(data.breedList);
 const dogsImages = JSON.parse(data.breedImages);
 const selectForm = document.querySelector(".mySelect");
-console.log(dogsList);
-console.log(dogsImages);
+// console.log(dogsList);
+// console.log(dogsImages);
 
 const findDogRase = function () {
   selectForm.innerHTML = Object.keys(dogsList.message).map(function (el) {
-    return `<option value= "${el}" class="mySelect__rase"> ${el} </option>`
-  })
-}
+    return `<option value= "${el}" class="mySelect__rase"> ${el} </option>`;
+  });
+};
 findDogRase();
 
-selectForm.addEventListener('change', (ra) => {
-  const sel = selectForm[selectForm.selectedIndex].value;
-  const str = dogsImages[sel];
-  const insertMain = document.createElement("main");
-  str.forEach(img => {
-    insertMain.innerHTML = `<img src=${img}/>;`;
-  });
+const photoList = document.getElementById("photos");
+selectForm.addEventListener("change", (e) => {
+  e.preventDefault();
+  const selectValue = selectForm[selectForm.selectedIndex].value;
+  const imagesList = dogsImages[selectValue];
+  console.log(imagesList);
+  photoList.innerHTML = imagesList
+    .map((img) => /* html */ `<li><img src="${img}" alt="Dog rase"/></li>`)
+    .join(",");
 });

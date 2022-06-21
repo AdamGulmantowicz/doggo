cf2821dc9e37dfa0a884b5598d79cb5425a01de8
TomaszFeszter <tomek.feszter.grupa@gmail.com>
2022-06-21 22:30:15 +0200

    zad2

--- js/index.js
+++ js/index.js
@@ -3,40 +3,44 @@ import data from "../data/data.js";
 const breedList = JSON.parse(data.breedList);
 const breedImages = JSON.parse(data.breedImages);
 const main = document.querySelector('main');
+const header = document.querySelector('header');
+
 
 function renderPage(renderContent) {
-    main.innerHTML = renderContent;
+    header.innerHTML = renderContent;
 }
 
 const renderMain = function () {
 
-    return `<form id="dogForm">
+    return /*html*/ `
+        <form id="dogForm">
             <select name="breed" id="select" form="dogs">
-            ${Object.keys(breedList.message).map(function (breed) {
-                return `<option value="${breed}">${breed}</option>`
-            }).join('')}
+                ${Object.keys(breedList.message).map(function (breed) {
+                    return `<option value="${breed}">${breed}</option>`
+                }).join('')}
             </select>
             <input type="submit" value="Submit">
-            </form>
-            <section class="gallery">
-            </section>`
-            ;
+        </form>`;
 }
 
 renderPage(renderMain());
 
-const form = document.getElementById('dogForm');
 
+const form = document.getElementById('dogForm');
+const gallerySection = document.getElementById('gallery')
+console.log(gallerySection)
 
 const renderImg = function () {
     form.addEventListener('submit', (e) => {
         e.preventDefault();
         const selectValue = select.options[select.selectedIndex].value;
-        const selectImg =  breedImages[selectValue];
-        const gallerySection = document.querySelector('gallery')
-        selectImg.forEach(path => {
-            gallerySection.innerHTML = `<img src=${path}>`
-        })
+        const selectImg = breedImages[selectValue];
+        console.log(selectImg)
+        gallerySection.innerHTML = selectImg.map((path) => /*html*/
+            `<div class="gallery__item">
+                <img class="gallery__img" src="${path}">
+            </div>`
+        ).join('');
     })
 }
 renderImg();

9e1aee585cf36752ef090deafa5da273f35b33df
TomaszFeszter <tomek.feszter.grupa@gmail.com>
2022-06-22 16:17:24 +0200

    add set for liked img

--- js/index.js
+++ js/index.js
@@ -29,7 +29,7 @@ renderPage(renderMain());
 
 const form = document.getElementById('dogForm');
 const gallerySection = document.getElementById('gallery')
-
+const favouritesSet = new Set();
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
@@ -41,11 +41,14 @@ form.addEventListener('submit', (e) => {
             <div class="like"></div>
         </div>`
     ).join('');
+
     const like = Array.from(document.querySelectorAll('.like'))
     like.forEach(element => {
         element.addEventListener('click', function () {
             element.classList.toggle('like--checked')
-            favourites.innerHTML = element.parentElement.outerHTML
+            if (!element.classList.contains('like--checked')) {
+                favouritesSet.add(element.parentElement.outerHTML);
+            }
         })
     })
 

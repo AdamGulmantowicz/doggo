b8ecc04bc2c5f78187a318a591d7c124d5e8a7b1
TomaszFeszter <tomek.feszter.grupa@gmail.com>
2022-06-29 17:41:59 +0200

    add localStorage, clear favs button

--- js/index.js
+++ js/index.js
@@ -42,28 +42,64 @@ form.addEventListener('submit', (e) => {
     gallerySection.innerHTML = renderImg.map((path) => /*html*/
         `<div class="card mb-m">
             <img class="card__img" src="${path}">
-            <button class="btn like ${favouritesSet.has(path) ? "": "btn--secondary"}">❤️</butt>
+            <button class="btn like ${localStorage.getItem(`${path}`) ? "": "btn--secondary"}">❤️</butt>
         </div>`
     ).join('');
-
-    const like = Array.from(document.querySelectorAll('.like'))
-    const renderFavs = function () {
-        favourites.innerHTML = Array.from(favouritesSet).map((path) => /*html*/
+    favourites.innerHTML = `<button id="clearBtn" class="btn" type="submit">Clear Favourites</button>` +
+        Object.keys(localStorage).map((path) => /*html*/
             `<div class="card mb-m">
             <img class="card__img" src="${path}">
         </div>`
         ).join('');
+    Object.keys(localStorage).forEach(el => {
+        favouritesSet.add(el)
+    })
+
+
+
+
+    const like = Array.from(document.querySelectorAll('.like'))
+
+
+    const renderFavs = function () {
+        favourites.innerHTML = `<button id="clearBtn" class="btn" type="submit">Clear Favourites</button>` +
+            Array.from(favouritesSet).map((path) => /*html*/
+                `<div class="card mb-m">
+            <img class="card__img" src="${path}">
+        </div>`
+            ).join('');
+
+        const clearBtn = document.getElementById('clearBtn');
+
+        clearBtn.addEventListener('click', (e) => {
+            localStorage.clear();
+            console.log(favouritesSet);
+            Array.from(favouritesSet).forEach(el => {
+                favouritesSet.delete(`${el}`)
+            })
+            console.log(favouritesSet);
+            like.forEach(el => {
+                if (!el.classList.contains('btn--secondary')) {
+                    el.classList.toggle('btn--secondary');
+                }
+            })
+            favourites.innerHTML = `<button id="clearBtn" class="btn" type="submit">Clear Favourites</button>`
+        })
     }
+    renderFavs();
     like.forEach((element, i) => {
         element.addEventListener('click', function () {
             const currentImg = breedImages[selectValue][i];
             if (!favouritesSet.has(currentImg)) {
                 element.classList.toggle('btn--secondary')
                 favouritesSet.add(currentImg)
+                localStorage.setItem(`${currentImg}`, `${currentImg}`);
+                console.log(Object.keys(localStorage))
                 renderFavs();
             } else {
                 element.classList.toggle('btn--secondary')
                 favouritesSet.delete(currentImg)
+                localStorage.removeItem(`${currentImg}`);
                 renderFavs()
             }
         })
